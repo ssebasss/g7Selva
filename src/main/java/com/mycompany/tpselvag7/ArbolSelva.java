0d035e4d75f4da39f2b4fdd9d8f95b72e2a553b4
BLACK1895 <miguel18.jmr13@gmail.com>
2025-06-10 16:48:07 -0300

    se modifico una de las opciones

--- src/main/java/com/mycompany/tpselvag7/ArbolSelva.java
+++ src/main/java/com/mycompany/tpselvag7/ArbolSelva.java
@@ -20,13 +20,13 @@ public class ArbolSelva {
         arbol[1] = "Decidis buscar comida. Seguir un rio (A) o buscar frutos (B)?";
         arbol[2] = "Exploras la zona. Ves una cueva (A) o escuchas ruidos extranos (B)?";
         arbol[3] = "Seguis el rio. Te parece potable. Beber (A) o seguir corriente (B)?";
-        arbol[4] = "Encontras frutos de colores. Comer (A) o guardarlos (B)?";
+        arbol[4] = "Encontras frutos de colores. Comer (A) o ignorarlos (B)?";
         arbol[5] = "Te acercas a la cueva. Entrar (A) o rodearla (B)?";
         arbol[6] = "Te acercas al ruido. Es un animal. Subir a un arbol (A) o correr (B)?";
         arbol[7] = "FIN: El agua estaba contaminada. No sobreviviste.";
         arbol[8] = "FIN: Descubriste una cascada con peces. Sobreviviste.";
         arbol[9] = "FIN: Frutos venenosos. No sobreviviste.";
-        arbol[10] = "FIN: Guardaste comida. Tenes energia para seguir.";
+        arbol[10] = "FIN: Evitaste ser envenenado";
         arbol[11] = "FIN: Dentro de la cueva habia un refugio abandonado. Descansas.";
         arbol[12] = "FIN: Al rodearla caiste por una pendiente. No sobreviviste.";
         arbol[13] = "El animal te persigue. Esconderse (A) o enfrentarlo (B)?";
